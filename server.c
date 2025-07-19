41b722d7647a28331bfffbf468b184aad5cd6409
totake-42 <taishi.micoluna@gmail.com>
2025-07-20 00:14:14 +0900

    update: add timeout and static clear

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: totake <totake@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 17:53:43 by totake            #+#    #+#             */
-/*   Updated: 2025/07/19 23:22:08 by totake           ###   ########.fr       */
+/*   Updated: 2025/07/20 00:11:22 by totake           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,28 +57,45 @@ void	setup_signal_handlers(void)
 		handle_errors("sigaction: failed to set handler for SIGUSR2");
 }
 
+void	process_signal(unsigned char *char_buf, int *bit_count)
+{
+	if (g_state.received_sig == SIGUSR1)
+		*char_buf |= (1 << (7 - *bit_count));
+	if (++(*bit_count) == 8)
+	{
+		write(1, char_buf, 1);
+		*char_buf = 0;
+		*bit_count = 0;
+	}
+	usleep(50);
+	if (kill(g_state.sender_pid, SIGUSR1) == -1)
+		handle_errors("kill: failed to send ACK");
+}
+
 int	main(void)
 {
 	static unsigned char	char_buf = 0;
 	static int				bit_count = 0;
+	int				usleep_count = 0;
 
 	setup_signal_handlers();
 	ft_printf("Server PID: %d\n", getpid());
 	while (1)
 	{
 		while (!g_state.signal_received)
-			usleep(100);
-		g_state.signal_received = 0;
-		if (g_state.received_sig == SIGUSR1)
-			char_buf |= (1 << (7 - bit_count));
-		if (++bit_count == 8)
 		{
-			write(1, &char_buf, 1);
-			char_buf = 0;
-			bit_count = 0;
+			usleep(100);
+			if (++usleep_count >= 10000)
+			{
+				write(2, "Error: Timeout\n", 15);
+				char_buf = 0;
+				bit_count = 0;
+				usleep_count = 0;
+			}
 		}
-		usleep(50);
-		if (kill(g_state.sender_pid, SIGUSR1) == -1)
-			handle_errors("kill: failed to send signal (SIGUSR1)");
+		g_state.signal_received = 0;
+		usleep_count = 0;
+		process_signal(&char_buf, &bit_count);
 	}
 }
+
